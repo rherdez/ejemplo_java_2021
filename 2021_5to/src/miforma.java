163d8a0ab22e86584ca02927b78695a0c7aa9561
herdezclase <herdez.clase@gmail.com>
2021-02-10 20:57:21 -0600

    $m

---
+++ 2021_5to/src/miforma.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rober
+ */
+public class miforma extends javax.swing.JFrame  {
+    
+    public miforma() {
+        initComponents();
+    }
+    
+     private void initComponents() {
+
+      
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Principal");
+        setExtendedState(6);
+
+     
+
+      
+
+        pack();
+    }// </editor-fold>  
+}
